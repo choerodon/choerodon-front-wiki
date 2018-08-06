71f6a4b7ae311292a2522174ff7fd654efd045c2
Zenger <qinsheng.wang@hand-china.com>
2018-08-06 21:30:15 +0800

    [IMP] Modify the style

--- wiki/src/app/wiki/locale/en_US.js
+++ wiki/src/app/wiki/locale/en_US.js
@@ -32,7 +32,7 @@ const enUS = {
     'wiki.operating': 'operating',
     'wiki.failed': 'Do not edit',
     'wiki.delete.space': 'Delete space',
-    'wiki.delete.tooltip': 'Are you sure you want to delete this page',
+    'wiki.delete.tooltip': 'Are you sure you want to delete this space',
 };
   export default enUS;
   
